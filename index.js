5c92c76dc20032a39a91ae77d49070bf536e0b65
ben riley <benriley@seattleacademy.org>
2017-05-09 14:57:30 -0700

    played with socket.io

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
@@ -7,8 +6,22 @@ const port = process.env.PORT || 1582;
 
 app.use(express.static(__dirname + '/public'));
 
-function onConnection(socket){
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+
+function onConnection(socket) {
+	// var bots =[];
+	// var bot1 = {};
+	// bot1.x = 42;
+	// bot1.y = 150;
+	// bot1.r = 20;
+	// bot1.theta = 0;
+	// bot1.color = "pink"; 
+	// bot1.emit = true;
+    function drawData(data) {
+    	console.log("data",data);
+        socket.broadcast.emit('drawing', data);
+        // socket.broadcast.emit('drawing', bot1);
+    }
+    socket.on('drawing', drawData);
 }
 
 io.on('connection', onConnection);
--- public/main.js
+++ public/main.js
@@ -8,14 +8,21 @@
   var context = canvas.getContext('2d');
 
   var current = {
-    color: 'black'
+    color: 'blue'
   };
   var drawing = false;
+  var myBot = {};
 
+  myBot.x = 100;
+  myBot.y = 200;
+  myBot.r = 15;
+  myBot.color = "orange";
+  myBot.theta = 0;
+  myBot.emit = true;
+
+  var body = document.getElementById("mybody");
   canvas.addEventListener('mousedown', onMouseDown, false);
-  canvas.addEventListener('mouseup', onMouseUp, false);
-  canvas.addEventListener('mouseout', onMouseUp, false);
-  canvas.addEventListener('mousemove', throttle(onMouseMove, 10), false);
+  body.addEventListener('keydown', onKeyDown, false);
 
   for (var i = 0; i < colors.length; i++){
     colors[i].addEventListener('click', onColorUpdate, false);
@@ -23,51 +30,47 @@
 
   socket.on('drawing', onDrawingEvent);
 
-  window.addEventListener('resize', onResize, false);
-  onResize();
+  // window.addEventListener('resize', onResize, false);
+  // onResize();
 
 
-  function drawLine(x0, y0, x1, y1, color, emit){
+  function drawBot(x,y,r,theta,color, emit){
     context.beginPath();
-    context.moveTo(x0, y0);
-    context.lineTo(x1, y1);
-    context.strokeStyle = color;
-    context.lineWidth = 2;
-    context.stroke();
+    context.arc(x,y,15,0,2*Math.PI);
+    context.fillStyle = color;
+    context.fill();
     context.closePath();
 
     if (!emit) { return; }
-    var w = canvas.width;
-    var h = canvas.height;
+
 
     socket.emit('drawing', {
-      x0: x0 / w,
-      y0: y0 / h,
-      x1: x1 / w,
-      y1: y1 / h,
-      color: color
+      x: x,
+      y: y,
+      r: r,
+      theta: theta,
+      color: color,
+      emit: emit
     });
   }
 
+
+
   function onMouseDown(e){
-    drawing = true;
-    current.x = e.clientX;
-    current.y = e.clientY;
+    console.log("e",e);
+    drawBot(e.clientX, e.clientY,15,0,current.color, true);
   }
 
-  function onMouseUp(e){
-    if (!drawing) { return; }
-    drawing = false;
-    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
-  }
 
-  function onMouseMove(e){
-    if (!drawing) { return; }
-    drawLine(current.x, current.y, e.clientX, e.clientY, current.color, true);
-    current.x = e.clientX;
-    current.y = e.clientY;
-  }
+  function onKeyDown(e){
+    console.log("e",e);
+    if(e.key == arrowUp){
+    
+  myBot.y = myBot.y -1;
 
+    }
+    drawBot(myBot.x, myBot.y,myBot.r,myBot.theta,myBot.color, true);
+  }
   function onColorUpdate(e){
     current.color = e.target.className.split(' ')[1];
   }
@@ -88,13 +91,8 @@
   function onDrawingEvent(data){
     var w = canvas.width;
     var h = canvas.height;
-    drawLine(data.x0 * w, data.y0 * h, data.x1 * w, data.y1 * h, data.color);
+    drawBot(data.x, data.y, data.r, data.theta, data.color,true);
   }
 
-  // make the canvas fill its parent
-  function onResize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  }
 
 })();
